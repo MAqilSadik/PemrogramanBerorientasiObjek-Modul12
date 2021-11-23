9da6fd1203658d2b191a5bcd6e33096b3863fe1c
Aqil Sadik <dmadaaqil@gmail.com>
2021-11-23 22:17:17 +0700

    Create Gold.java

---
+++ RentalConsole/src/rentalconsole/Gold.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentalconsole;
+
+/**
+ *
+ * @author User
+ */
+public class Gold extends Silver {
+   int cashback;
+    
+    Gold(){
+       biaya =  30000;
+       point = 5;
+       diskon = 2;
+       cashback = 5000;
+    }
+
+}
